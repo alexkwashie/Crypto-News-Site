88e4994e1c8c93d553b26657fd4a84815962f0ff
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-02-12 23:05:06 +0000

    Adding Api request

--- Crypto/views.py
+++ Crypto/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
+    import requests # this allows us to call for reqest form webpage(use pip3 install to use)
+    import json # allows us to work with the API objects
+    api_request = request.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
+    api = json.load(api_request.content)
     return render(request, 'index.html', {})
