c8462546f3bbd345ad797dd4be43bc25fda8e4b9
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-02-16 22:36:51 +0000

    using api data

--- Crypto/views.py
+++ Crypto/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html', {})
+    import requests #use requests module(use pip3 to install json)
+    import json #use json module (use pip3 to install json)
+    api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")#get api from site
+    api1 = json.loads(api_request.content)# convert api content to json
+    return render(request, 'index.html', {'api': api1})
