fbdd420ba0a36624ae2e6387408a4a383c1399cf
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-02-20 21:59:54 +0000

    working on price data  search1

--- Crypto/views.py
+++ Crypto/views.py
@@ -14,13 +14,21 @@ def home(request):
     price_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP,ETH,EOS,LTC,QTUM,ZEC,ETC&tsyms=USD,EUR")#get price api from site
     price_api = json.loads(price_request.content)# convert api content to json
 
-    return render(request, 'index.html', {'api': api1,'price':price_api})
+    return render(request, 'index.html', {'api': api1, 'price':price_api})
 
 
 def prices(request):
+        #this a functionality to search through the api for info. on coins on the prices.html page
+
+       #Retrive price data
     if request.method == 'POST': #if it gets request.method from html
-        search1 = request.POST['search']
-        return render(request, 'prices.html', {'search': search1})
+        import requests #use requests module(use pip3 to install json)
+        import json #use json module (use pip3 to install json)
+
+        search1 = request.POST['search'] #coin search from the search input
+        search_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms="+search1+"&tsyms=USD")
+        search_output = json.loads(search_request.content)# convert api content to json
+        return render(request, 'prices.html', {'search': search1, 'searchoutput': search_output})
     else:
         return render(request, 'prices.html', {})
 
